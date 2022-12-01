263ee7003911f5390844937d48364bef1cd75e56
amilcaralex97 <amilcaralex97@gmail.com>
2022-11-30 23:31:01 -0600

    api stack

--- lib/sportdrive-serverless-stack.ts
+++ lib/sportdrive-serverless-stack.ts
@@ -1,11 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class SportdriveServerlessStack extends cdk.Stack {
+  private api = new RestApi(this, 'sport-drive-api');
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    
+    
     // The code that defines your stack goes here
 
     // example resource
