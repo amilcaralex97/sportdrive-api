804b725b21fb8bc0b087e897eed2fc9d644c752f
Amilcar Sanchez <amilcaralex97@gmail.com>
2022-12-01 09:56:45 -0600

    lambda class

--- lib/generic-lambda.ts
+++ lib/generic-lambda.ts
@@ -1,3 +1,18 @@
-class GenericLambda{
-    
+import { Function } from 'aws-cdk-lib/aws-lambda';
+
+type LambdaProps = {
+	dbName: string;
+	method: string;
+};
+
+class GenericLambda {
+	constructor(props: LambdaProps) {
+		this.initialize(props);
+	}
+
+	private initialize(props: LambdaProps) {
+		this.createLambda(props);
+	}
+
+	private createLambda(props: LambdaProps) {}
 }
