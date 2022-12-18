7da72e094822f354b8a349b0e5bf58e5a1f013e0
Amilcar Sanchez <amilcaralex97@gmail.com>
2022-12-17 23:30:27 -0600

    connection to db

--- src/infrastructure/connection.ts
+++ src/infrastructure/connection.ts
@@ -1,8 +1,8 @@
-import { createConnection } from 'mongoose';
+import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
 dotenv.config({ path: __dirname + '/.env' });
 
-let conn: any = null;
+let conn = null;
 
 const uri = `mongodb+srv://${process.env.AWS_ACCESS_KEY}:${process.env.AWS_SECRET_KEY}@sportdrive-dev-cluster.dkvgtfh.mongodb.net/?authSource=%24external&authMechanism=MONGODB-AWS&retryWrites=true&w=majority`;
 
@@ -10,12 +10,14 @@ async function handler(event: any, context: any) {
 	context.callbackWaitsForEmptyEventLoop = false;
 
 	if ((conn = null)) {
-		conn = createConnection(uri, {
-			serverSelectionTimeoutMS: 5000,
-		});
+		conn = mongoose
+			.connect(uri, {
+				serverSelectionTimeoutMS: 5000,
+			})
+			.then(() => mongoose);
 	}
 
-	await conn.asPromise();
+	return conn;
 }
 
 export { handler };
