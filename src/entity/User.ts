0de199c218d734c4ece7665d4db464c05aea99bc
Amilcar Sanchez <amilcaralex97@gmail.com>
2022-12-19 09:11:59 -0600

    User model

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,15 +1,18 @@
 import { Schema, model, Types } from 'mongoose';
 
 interface IUser {
+	userId: string;
 	username: string;
 	name: string;
 	password: string;
 	roleId: Types.ObjectId;
 }
 
-export const userSchema = new Schema<IUser>({
+const userSchema = new Schema<IUser>({
 	username: { type: String, required: true },
 	name: { type: String, required: true },
 	password: { type: String, required: true },
 	roleId: { type: Schema.Types.ObjectId, required: true },
 });
+
+export const User = model('User', userSchema);
