15e0c4aee2322bbcddaeba29571a7224f091e7c3
Amilcar Sanchez <amilcaralex97@gmail.com>
2022-12-25 09:50:01 -0600

    receipt entity wip

---
+++ src/entity/Receipt.ts
@@ -0,0 +1,168 @@
+import { Schema, model } from 'mongoose';
+import { v4 as uuidv4 } from 'uuid';
+
+interface IReceipt {
+	roleId: String;
+	userAccess: number;
+	receiptAccess: number;
+}
+
+const receiptSchema = new Schema(
+	{
+		clientName: {
+			type: String,
+		},
+		clientPhone: {
+			type: String,
+		},
+		clientEmail: {
+			type: String,
+		},
+		receiptDate: {
+			type: String,
+		},
+		arriveTime: {
+			type: String,
+		},
+		installationTime: {
+			type: String,
+		},
+		vehicleBrand: {
+			type: String,
+		},
+		vehicleModel: {
+			type: String,
+		},
+		vehicleTransmission: {
+			type: String,
+		},
+		vehicleType: {
+			type: String,
+		},
+		gasType: {
+			type: String,
+		},
+		gasTank: {
+			type: String,
+		},
+		vehicleVin: {
+			type: String,
+		},
+		vehicleYear: {
+			type: String,
+		},
+		vehicleEngine: {
+			type: String,
+		},
+		vehicleKm: {
+			type: String,
+		},
+		vehicleCylinder: {
+			type: String,
+		},
+		vehiclePlate: {
+			type: String,
+		},
+		topSport: {
+			type: Boolean,
+			default: false,
+		},
+		topBoost: {
+			type: Boolean,
+			default: false,
+		},
+		topBoostPart: {
+			type: String,
+		},
+		topBoostPro: {
+			type: Boolean,
+			default: false,
+		},
+		topBoostProPart: {
+			type: String,
+		},
+		otherProduct: {
+			type: String,
+		},
+		checkEngine: {
+			type: Boolean,
+			default: false,
+		},
+		epc: {
+			type: Boolean,
+			default: false,
+		},
+		abs: {
+			type: Boolean,
+			default: false,
+		},
+		asr: {
+			type: Boolean,
+			default: false,
+		},
+		airBag: {
+			type: Boolean,
+			default: false,
+		},
+		tirePressure: {
+			type: Boolean,
+			default: false,
+		},
+		antiTheft: {
+			type: Boolean,
+			default: false,
+		},
+		battery: {
+			type: Boolean,
+			default: false,
+		},
+		brakes: {
+			type: Boolean,
+			default: false,
+		},
+		reqService: {
+			type: Boolean,
+			default: false,
+		},
+		vehicleStatusDescription: {
+			type: String,
+		},
+		scanBeforeDescription: {
+			type: String,
+		},
+		reviewVehicleStart: {
+			type: String,
+		},
+		reviewVehicleResponse: {
+			type: String,
+		},
+		reviewFunctionality: {
+			type: String,
+		},
+		reviewService: {
+			type: String,
+		},
+		reviewTime: {
+			type: String,
+		},
+		reviewRecomendation: {
+			type: String,
+		},
+		receiptUrl: {
+			type: String,
+		},
+		draft: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	{
+		_id: false,
+		receiptId: {
+			type: String,
+			default: uuidv4,
+		},
+	}
+);
+
+export const Receipt = model<IReceipt>('Receipt', receiptSchema);
