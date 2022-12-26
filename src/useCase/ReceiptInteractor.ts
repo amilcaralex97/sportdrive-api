f4847a6b2db33166d4bedba52d6f02da407ae9f1
Amilcar Sanchez <amilcaralex97@gmail.com>
2022-12-25 22:33:55 -0600

    Receipt Interactor mockup

---
+++ src/useCase/ReceiptInteractor.ts
@@ -0,0 +1,26 @@
+class ReceiptInteractor {
+	/**
+	 * GetReceipts
+	 */
+	public GetReceipts() {}
+
+	/**
+	 * GetReceipt
+	 */
+	public GetReceipt() {}
+
+	/**
+	 * CreateReceipt
+	 */
+	public CreateReceipt() {}
+
+	/**
+	 * UpdateReceipt
+	 */
+	public UpdateReceipt() {}
+
+	/**
+	 * DeleteReceipt
+	 */
+	public DeleteReceipt() {}
+}
