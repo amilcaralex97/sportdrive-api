a70a25ff4265cb44ea24bf60808c36a8dbccfaf0
Amilcar Sanchez <amilcaralex97@gmail.com>
2022-12-25 22:34:09 -0600

    Role Interactor mockup

---
+++ src/useCase/RoleInteractor.ts
@@ -0,0 +1,26 @@
+class RoleInteractor {
+	/**
+	 * GetRoles
+	 */
+	public GetRoles() {}
+
+	/**
+	 * GetRole
+	 */
+	public GetRole() {}
+
+	/**
+	 * CreateRole
+	 */
+	public CreateRole() {}
+
+	/**
+	 * UpdateRole
+	 */
+	public UpdateRole() {}
+
+	/**
+	 * DeleteRole
+	 */
+	public DeleteRole() {}
+}
