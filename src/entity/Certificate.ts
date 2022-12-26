3b29c9bd87b7b3d0e2c6ae1e44095f09fe88c371
Amilcar Sanchez <amilcaralex97@gmail.com>
2022-12-25 22:13:12 -0600

    Certificate entity

---
+++ src/entity/Certificate.ts
@@ -0,0 +1,24 @@
+import { Schema, model, Types } from 'mongoose';
+import { v4 as uuidv4 } from 'uuid';
+
+interface ICertificate {
+	certificateId: string;
+	certificateUrl: string;
+	receiptId: Types.ObjectId;
+}
+
+const certificateSchema = new Schema(
+	{
+		certificateUrl: { type: String, required: true },
+		receiptId: { type: Schema.Types.ObjectId, ref: 'Receipt' },
+	},
+	{
+		_id: false,
+		certificateId: {
+			type: String,
+			default: uuidv4,
+		},
+	}
+);
+
+export const Role = model<ICertificate>('Certificate', certificateSchema);
