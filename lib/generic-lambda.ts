b29da340ad4559868dd68a591f176bddeb6b2803
Amilcar Sanchez <amilcaralex97@gmail.com>
2022-12-27 14:39:44 -0600

    generic lambda class

--- lib/generic-lambda.ts
+++ lib/generic-lambda.ts
@@ -3,17 +3,17 @@ import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { join } from 'path';
 
 type LambdaProps = {
-    env: string
+	env: string;
 	model: string;
 	method: string;
 };
 
-class GenericLambda {
+export class GenericLambda {
 	private stack: Stack;
 	private props: LambdaProps;
-	constructor(stack:Stack, props: LambdaProps) {
-        this.stack =stack
-        this.props = props
+	constructor(stack: Stack, props: LambdaProps) {
+		this.stack = stack;
+		this.props = props;
 		this.initialize();
 	}
 
@@ -22,9 +22,19 @@ class GenericLambda {
 	}
 
 	private createLambda() {
-        const lambdaId = `${this.props.env}-${this.props.model}`;
-        return new NodejsFunction(this.stack,lambdaId,{
-
-        })
-    }
+		const lambdaId = `${this.props.env}-${this.props.model}`;
+		return new NodejsFunction(this.stack, lambdaId, {
+			entry: join(
+				__dirname,
+				'..',
+				'src',
+				'infrastructure',
+				'Lambda',
+				this.props.model,
+				this.props.method
+			),
+			handler: 'handler',
+			functionName: lambdaId,
+		});
+	}
 }
