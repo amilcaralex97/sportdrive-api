ce17d371f39d0750492e66a4132cd517523f36e5
Amilcar Sanchez <amilcaralex97@gmail.com>
2022-12-30 10:55:27 -0600

    Role interactor wip

---
+++ src/useCase/__test__/RoleInteractor.spec.ts
@@ -0,0 +1,7 @@
+describe('RoleInteractor', () => {
+	it('Should seed admin role if no role found', async () => {});
+
+	it('Should create role', async () => {});
+
+	it('Should throw an error when it fails to create a role', async () => {});
+});
