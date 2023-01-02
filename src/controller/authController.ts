78be23d46d54fa7c3305064ce6e0f81c6752408e
Amílcar Sánchez <45496427+amilcaralex97@users.noreply.github.com>
2023-01-02 10:02:05 -0600

    signin wip

--- src/controller/authController.ts
+++ src/controller/authController.ts
@@ -1,6 +1,20 @@
+import { ApiDefinition } from 'aws-cdk-lib/aws-apigateway';
+import { APIGatewayEvent } from 'aws-lambda';
+
 export class AuthController {
+	private event: APIGatewayEvent;
+
+	constructor(event: APIGatewayEvent) {
+		this.event = event;
+	}
+
 	/**
 	 * signIn
 	 */
-	public signIn() {}
+	public async signIn() {
+		let body: object;
+		if (this.event.body) {
+			body = JSON.parse(this.event.body);
+		}
+	}
 }
