c97672fb8e1de3cede30b4c52f705c2d8a053a64
Amílcar Sánchez <45496427+amilcaralex97@users.noreply.github.com>
2023-01-08 13:03:42 -0600

    auth interactor wip

--- src/useCase/AuthInteractor/AuthInteractor.ts
+++ src/useCase/AuthInteractor/AuthInteractor.ts
@@ -1,6 +1,27 @@
+import { APIGatewayEvent } from 'aws-lambda';
+import { AuthController } from '../../controller/AuthController/AuthController';
+import { SignInDTO } from '../../controller/AuthController/AuthControllerTypes';
+
 export class AuthInteractor implements IAuthInteractor {
+	private event: APIGatewayEvent;
+	constructor(event: APIGatewayEvent) {
+		this.event = event;
+	}
+
 	/**
 	 * Login
 	 */
-	public Login() {}
+	public async Login() {
+		let loginDTO: SignInDTO;
+		try {
+			if (process.env.SEED_PASS) {
+			}
+
+			const auth = new AuthController(this.event);
+			loginDTO = await auth.signIn();
+		} catch (error) {
+			return { status: 500, message: 'Error en login' };
+		}
+		return loginDTO;
+	}
 }
--- src/useCase/AuthInteractor/AuthInteractorTypes.ts
+++ src/useCase/AuthInteractor/AuthInteractorTypes.ts
@@ -1,3 +1,3 @@
 interface IAuthInteractor {
-	Login: () => any;
+	Login: () => Promise<any>;
 }
