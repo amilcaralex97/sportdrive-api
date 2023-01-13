1c3a53900ad26aeb3d87fadadbb20694ed8e763d
Amilcar Sanchez <amilcaralex97@gmail.com>
2023-01-12 20:00:06 -0600

    user test cases

--- src/controller/UserController/__test__/userController.spec.ts
+++ src/controller/UserController/__test__/userController.spec.ts
@@ -1,5 +1,21 @@
 describe('User Controller', () => {
-	it('Should create an user', async () => {});
+	describe('createUser', () => {
+		it('Should create an user', async () => {});
+		it('Should catch error while creating an user', async () => {});
+	});
 
-	it('Should catch error while creating an user', async () => {});
+	describe('fetchUsers', () => {
+		it('Should return all users', async () => {});
+		it('Should catch error if it fails', async () => {});
+	});
+
+	describe('fetchUser', () => {
+		it('Should return a users', async () => {});
+		it('Should catch error if it fails', async () => {});
+	});
+
+	describe('updateUser', () => {
+		it('Should return update an user', async () => {});
+		it('Should catch error if it fails', async () => {});
+	});
 });
