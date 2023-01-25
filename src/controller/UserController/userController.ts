ce0569b4d829ed1c2ad25fcf97562ce30f0b8956
Amílcar Sánchez <45496427+amilcaralex97@users.noreply.github.com>
2023-01-25 09:29:55 -0600

    user controller  db instance fix

--- src/controller/UserController/userController.ts
+++ src/controller/UserController/userController.ts
@@ -1,12 +1,14 @@
 import mongoose from 'mongoose';
-import { User } from '../../entity/User';
+import { userSchema } from '../../entity/User';
 import { CreateUserRequest } from './UserControllerTypes';
 
 export class UserController {
 	private userProps: CreateUserRequest;
 	private db: typeof mongoose;
+	private userModel;
 	constructor(userProps: CreateUserRequest, db: typeof mongoose) {
 		this.userProps = userProps;
+		this.userModel = db.model('User', new mongoose.Schema(userSchema));
 	}
 
 	/**
@@ -15,7 +17,7 @@ export class UserController {
 	public async fetchUsers() {
 		let users;
 		try {
-			users = await User.find();
+			users = await this.userModel.find();
 		} catch (error) {
 			return { status: 500, message: 'Error al obtener los usuarios' };
 		}
@@ -37,7 +39,7 @@ export class UserController {
 	public async createUser() {
 		let user;
 		try {
-			user = new User(this.userProps);
+			user = new this.userModel(this.userProps);
 			user = await user.save();
 		} catch (error) {
 			return {
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -2,28 +2,28 @@ import { Schema, model, Types } from 'mongoose';
 import { v4 as uuidv4 } from 'uuid';
 
 export interface IUser {
-	userId?: string;
-	username: string;
+	userName: string;
 	name: string;
 	password: string;
-	roleId: Types.ObjectId;
+	roleId: string;
+	userId: string;
+	createdAt?: Date;
+	updatedAt?: Date;
 }
 
-const userSchema = new Schema(
+export const userSchema = new Schema<IUser>(
 	{
 		userName: { type: String, required: true },
 		name: { type: String, required: true },
 		password: { type: String, required: true },
 		roleId: { type: String, ref: 'Role' },
-	},
-	{
-		_id: false,
 		userId: {
 			type: String,
 			default: uuidv4,
 		},
+	},
+	{
+		_id: false,
 		timestamps: true,
 	}
 );
-
-export const User = model<IUser>('User', userSchema);
