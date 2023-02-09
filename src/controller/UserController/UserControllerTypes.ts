217890454c324c9f0ddac4a2da567d114f00b24a
Amílcar Sánchez <amilcaralex97@gmail.com>
2023-02-08 23:07:53 -0600

    user types change

--- src/controller/UserController/UserControllerTypes.ts
+++ src/controller/UserController/UserControllerTypes.ts
@@ -1,33 +1,35 @@
 export interface IUser {
-	userName: string;
-	name: string;
-	password: string;
-	roleId: string;
-	userId: string;
-	createdAt: Date;
-	updatedAt: Date;
+  userName: string;
+  name: string;
+  password: string;
+  roleId: string;
+  userId: string;
+  createdAt: string;
+  updatedAt: string;
 }
 
 export type CreateUserRequest = {
-	userName?: string;
-	name?: string;
-	password?: string;
-	roleId?: string;
-	userId?: string;
+  userName?: string;
+  name?: string;
+  password?: string;
+  roleId?: string;
+  userId?: string;
 };
 
 export type UserDTO = {
-	status: number;
-	message: string;
-	roles?: IUser[];
-	role?: IUser;
-	userId?: string;
+  status: number;
+  message: string;
+  roles?: IUser[];
+  role?: IUser;
+  userId?: string;
+  updatedAt?: string;
+  createdAt?: string;
 };
 
 export interface IUserController {
-	fetchUsers: () => Promise<UserDTO>;
-	fetchUser: () => Promise<UserDTO>;
-	fetchUserByUsername: () => Promise<UserDTO>;
-	createUser: () => Promise<UserDTO>;
-	updateUser: () => Promise<UserDTO>;
+  fetchUsers: () => Promise<UserDTO>;
+  fetchUser: () => Promise<UserDTO>;
+  fetchUserByUsername: () => Promise<UserDTO>;
+  createUser: () => Promise<UserDTO>;
+  updateUser: () => Promise<UserDTO>;
 }
